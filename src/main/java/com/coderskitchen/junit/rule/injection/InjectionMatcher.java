0a61531d21a3c99719310cd2744085fb76ef97ab
Peter Daum <daum@tyntec.com>
2013-05-05 08:49:05 +0200

    First classes and tests for autoinjection

--- src/main/java/com/coderskitchen/junit/rule/injection/InjectionMatcher.java
+++ src/main/java/com/coderskitchen/junit/rule/injection/InjectionMatcher.java
@@ -1,4 +1,32 @@
 package com.coderskitchen.junit.rule.injection;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InjectionMatcher {
+  private Set<InjectionMatch> matches;
+
+  public InjectionMatcher() {
+    matches = new HashSet<InjectionMatch>();
+  }
+
+  public InjectionMatcher forClassUnderTest(final Class classUnderTest) {
+    return this;
+  }
+
+  public InjectionMatcher atTestClass(final Class testClass) {
+    return this;
+  }
+
+  public InjectionMatcher match() {
+    return this;
+  }
+
+  public InjectionMatcher inject() {
+    return this;
+  }
+
+  public Set<InjectionMatch> getMatches() {
+    return matches;
+  }
 }
--- src/test/java/com/coderskitchen/junit/rule/injection/InjectionMatcherTest.java
+++ src/test/java/com/coderskitchen/junit/rule/injection/InjectionMatcherTest.java
@@ -1,4 +1,33 @@
 package com.coderskitchen.junit.rule.injection;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+
+import java.util.Set;
+
 public class InjectionMatcherTest {
+
+  InjectionMatcher cut = new InjectionMatcher();
+
+  @Test
+  public void nothingToInject() {
+    Class cutter = EmptyClass.class;
+    Class cutTest = NothingToInjectTestCase.class;
+    cut.forClassUnderTest(cutter);
+    cut.atTestClass(cutTest);
+    cut.match();
+    Set<InjectionMatch> injectionMatches = cut.getMatches();
+    assertThat(injectionMatches.isEmpty(), is(true));
+  }
+
+  @Test
+  public void noMatchesFound() {
+    Class cutter = EmptyClass.class;
+    Class cutTest = NothingMatchingToInject.class;
+    cut.forClassUnderTest(cutter);
+    cut.atTestClass(cutTest);
+    cut.match();
+    assertThat(cut.getMatches().isEmpty(), is(true));
+  }
 }
