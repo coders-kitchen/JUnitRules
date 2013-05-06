128ac4fc5ec7863c98944adccc2d861b6e46d3e5
Peter Daum <peter.daum.80@gmail.com>
2013-05-06 22:38:14 +0200

    Completed InjectionMatcher

--- src/main/java/com/coderskitchen/junit/rule/injection/InjectionMatcher.java
+++ src/main/java/com/coderskitchen/junit/rule/injection/InjectionMatcher.java
@@ -1,32 +1,72 @@
 package com.coderskitchen.junit.rule.injection;
 
+import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Set;
 
 public class InjectionMatcher {
   private Set<InjectionMatch> matches;
+  private Field[] cutFields;
+  private Field[] testFields;
+  private Set<String> alreadyMatchedFields;
 
   public InjectionMatcher() {
     matches = new HashSet<InjectionMatch>();
+    alreadyMatchedFields = new HashSet<String>();
   }
 
   public InjectionMatcher forClassUnderTest(final Class classUnderTest) {
+    cutFields = classUnderTest.getDeclaredFields();
     return this;
   }
 
   public InjectionMatcher atTestClass(final Class testClass) {
+    testFields = testClass.getDeclaredFields();
     return this;
   }
 
-  public InjectionMatcher match() {
+  public InjectionMatcher calculateMatches() {
+    matchFields();
+    addMissingMockFields();
+
     return this;
   }
 
-  public InjectionMatcher inject() {
-    return this;
+  private void matchFields() {
+    for (Field cutField : cutFields) {
+      for (Field testField : testFields) {
+        addMatchIfFieldsAreEqual(cutField, testField);
+      }
+    }
+  }
+
+  private void addMatchIfFieldsAreEqual(final Field cutField, final Field testField) {
+    String testFieldName = testField.getName();
+    String cutFieldName = cutField.getName();
+    if (areNamesEqual(testFieldName, cutFieldName) && areTypesEqual(cutField, testField)) {
+      alreadyMatchedFields.add(testFieldName);
+      matches.add(new InjectionMatch(testFieldName, cutFieldName));
+    }
+  }
+
+  private void addMissingMockFields() {
+    for (Field testField : testFields) {
+      if (alreadyMatchedFields.contains(testField.getName()))
+        continue;
+      matches.add(new InjectionMatch(testField.getName(), null));
+    }
   }
 
   public Set<InjectionMatch> getMatches() {
+
     return matches;
+  }
+
+  private boolean areNamesEqual(final String testFieldName, final String cutFieldName) {
+    return testFieldName.contains(cutFieldName);
+  }
+
+  private boolean areTypesEqual(final Field cutField, final Field testField) {
+    return cutField.getType().equals(testField.getType());
   }
 }
