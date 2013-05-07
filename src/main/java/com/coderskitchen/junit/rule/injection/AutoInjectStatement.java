38c3ee7d09ad7708b2a28e9bce2c1a4d42f1c5f6
Peter Daum <peter.daum.80@gmail.com>
2013-05-07 06:26:56 +0200

    Changed InjectionMatch from String to Field

--- src/main/java/com/coderskitchen/junit/rule/injection/AutoInjectStatement.java
+++ src/main/java/com/coderskitchen/junit/rule/injection/AutoInjectStatement.java
@@ -4,6 +4,24 @@ import org.junit.runners.model.Statement;
 
 public class AutoInjectStatement extends Statement {
 
+  private final InjectionMatcher matcher;
+  private final Statement surrounded;
+
+  public AutoInjectStatement(InjectionMatcher matcher, Statement surrounded) {
+    this.surrounded = surrounded;
+    this.matcher = matcher;
+  }
+
+  public AutoInjectStatement forClassUnderTest(Object classUnderTest) {
+    matcher.forClassUnderTest(classUnderTest.getClass());
+    return this;
+  }
+
+  public AutoInjectStatement atTestClass(Object testClass) {
+    matcher.atTestClass(testClass.getClass());
+    return this;
+  }
+
   @Override
   public void evaluate() throws Throwable {
 
